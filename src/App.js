092f2e4da672b58c2be46ffa09277c1c0836b10c
Fahim Shahriar <https://github.com/fahim-42>
2021-10-18 03:01:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2 className="bg-primary text-white border rounded">SK Diagnostics</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCSjq9YFB5waChnrIRfobO2hpXH8krNVvM",
+    authDomain: "sk-diagnostics.firebaseapp.com",
+    projectId: "sk-diagnostics",
+    storageBucket: "sk-diagnostics.appspot.com",
+    messagingSenderId: "754105614282",
+    appId: "1:754105614282:web:a985ad0a5c16f491eb9933"
+};
+
+export default firebaseConfig;
